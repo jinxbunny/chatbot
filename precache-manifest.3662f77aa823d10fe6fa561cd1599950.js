b5181202494efa353fae15c7609a885c95ec6283
jinxbunny <info@jmkit.com>
2020-06-01 20:56:44 +0100

    Updates

---
+++ precache-manifest.3662f77aa823d10fe6fa561cd1599950.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "26f3ef7d6cb119d7a6d7af943fcc1d1f",
+    "url": "/chatbot/index.html"
+  },
+  {
+    "revision": "b248cdef798c934a50d5",
+    "url": "/chatbot/static/css/main.d119ee85.chunk.css"
+  },
+  {
+    "revision": "5343b50dc10171dbeced",
+    "url": "/chatbot/static/js/2.e69239fb.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/chatbot/static/js/2.e69239fb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b248cdef798c934a50d5",
+    "url": "/chatbot/static/js/main.d34dfc82.chunk.js"
+  },
+  {
+    "revision": "4b6b4c93f134e5e03cdb",
+    "url": "/chatbot/static/js/runtime-main.3c8cc0ae.js"
+  }
+]);
